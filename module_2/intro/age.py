d9853d3b5bd1f68bd63a32db0d670fcd9dc5f904
Nyior <nyior.clement@aun.edu.ng>
2021-09-23 14:45:32 +0100

    add conditionals and loops

---
+++ module_2/intro/age.py
@@ -0,0 +1,11 @@
+# Age comparison
+
+age = int(input("What is your age: "))
+
+if age >= 18:
+    print("you're an adult")
+elif age < 18:
+    print("you're a minor")
+else:
+    print("invalid input")
+
