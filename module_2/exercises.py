2ffa7c626f9a635b94e74b9da2484dec7f108aed
raizo07 <agadaraizo@gmail.com>
2021-09-25 06:05:17 +0100

    module 2 solutions

--- module_2/exercises.py
+++ module_2/exercises.py
@@ -4,3 +4,40 @@
 
     Do sturvs here to demonstrate what you've learnt and open a pull request.
 """
+# Operations on strings and lists 
+
+"""
+A python string can be declared either 
+by using a single quote or by using double quotes.
+Strings are immutable but can be accesses and their indexing starts from 0
+
+"""
+
+# Example 
+
+statement ='Dog is an animal'
+print(statement)
+
+# Using len to get the length of characters
+
+ name = 'Bicycle'
+ len(name)
+
+ # Indexing
+
+  name = 'bicycle'
+  name[2] # It returns 2 as the answer
+
+# Slicing a string
+
+# sometimes you may want to display only a part of a string
+
+name = 'bicycle'
+name[3:5] # It gives 'yc'
+
+
+# the str() converts any data type into a string
+
+str(5+6y)
+
+
